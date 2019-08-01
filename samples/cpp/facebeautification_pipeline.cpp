efad8d88d06d6040171d7be001ceaf4103fed296
OrestChura <orest.chura@intel.com>
2019-08-01 14:24:51 +0300

    - Two-faces interaction bug is fixed (finally) - The feature to get the face box/facial landmarks drawing on/off by the command line arguments is added - The non-static function warning is solved - Some minor issues

--- samples/cpp/facebeautification_pipeline.cpp
+++ samples/cpp/facebeautification_pipeline.cpp
@@ -1,4 +1,4 @@
-//  This sample is an OpenCV implementation of a face beautifiaction algorythm.
+//  This sample is an OpenCV implementation of a face beautification algorythm.
 //
 //  The sample uses two pretrained OpenVINO networks so the OpenVINO package has to be preinstalled.
 //  Please install topologies described below using downloader.py
@@ -16,7 +16,7 @@ using namespace cv::dnn;
 
 #include <iostream>
 
-void unsharpMask(const Mat& src, Mat& dst, int sigma, double strength)
+static void unsharpMask(InputArray src, OutputArray dst, int sigma, double strength)
 {
     std::vector<Mat> chlsInput, chlsOut;
     split(src, chlsInput);
@@ -35,21 +35,24 @@ void unsharpMask(const Mat& src, Mat& dst, int sigma, double strength)
 
 int main(int argc, char** argv)
 {
-    const std::string winName = "FaceBeautificator";
-    namedWindow(winName, WINDOW_NORMAL);
-    namedWindow("Input", WINDOW_NORMAL);
+    const std::string winFB = "FaceBeautificator";
+    const std::string winInput = "Input";
+    namedWindow(winFB, WINDOW_NORMAL);
+    namedWindow(winInput, WINDOW_NORMAL);
     const Scalar clrGreen(0, 255, 0);
     const Scalar clrYellow(0, 255, 255);
     const Scalar clrBlack(0, 0, 0);
     const double pi = 3.1415926535897;
 
     CommandLineParser parser(argc, argv,
-     "{ help  h              | |     Print the help message. }"
+     "{ help         h       | |     Print the help message. }"
      "{ facestruct           | |     Full path to a Face detection model structure file (for example, .xml file).}"
      "{ faceweights          | |     Full path to a Face detection model weights file (for example, .bin file).}"
      "{ landmstruct          | |     Full path to a facial Landmarks detection model structure file (for example, .xml file).}"
      "{ landmweights         | |     Full path to a facial Landmarks detection model weights file (for example, .bin file).}"
-     "{ input          i     | |     Full path to an input image or a video file. Skip this argument to capture frames from a camera.}"
+     "{ input        i       | |     Full path to an input image or a video file. Skip this argument to capture frames from a camera.}"
+     "{ boxes        b       |false| Set true if want to draw face boxes in the \"Input\" window.}"
+     "{ landmarks    l       |false| Set true if want to draw facial landmarks in the \"Input\" window.}"
      );
 
     parser.about("Use this script to run face beautification algorythm.");
@@ -66,6 +69,9 @@ int main(int argc, char** argv)
     const std::string landmXmlPath = parser.get<std::string>("landmstruct");
     const std::string landmBinPath = parser.get<std::string>("landmweights");
 
+    const bool flgBoxes = parser.get<bool>("boxes");
+    const bool flgLandmarks = parser.get<bool>("landmarks");
+
     //Models' definition & initialization
     Net faceNet = readNet(faceXmlPath, faceBinPath);
     const unsigned int faceObjectSize = 7;
@@ -80,14 +86,17 @@ int main(int argc, char** argv)
     //Input
     VideoCapture cap;
     if (parser.has("input"))
+    {
         cap.open(parser.get<String>("input"));
+    }
     else if (!cap.open(0))
-            {
-                std::cout << "No input available" << std::endl;
-                return 1;
-            }
+    {
+        std::cout << "No input available" << std::endl;
+        return 1;
+    }
 
     Mat img;
+
     while (waitKey(1) < 0)
     {
         cap >> img;
@@ -96,16 +105,18 @@ int main(int argc, char** argv)
            waitKey();
            break;
         }
+        Mat imgDraw;
+        img.copyTo(imgDraw);
 
         //Infering Face detector
         faceNet.setInput(blobFromImage(img, 1.0, Size(faceCols, faceRows)));
         Mat faceOut = faceNet.forward();
 
-        Mat mskFaces(img.rows,img.cols, CV_8UC3, Scalar(0, 0, 0));
+        Mat mskNoFaces(img.rows,img.cols, CV_8UC3, Scalar(1, 1, 1));
         Mat mskBlurs(img.rows,img.cols, CV_8UC3, Scalar(0, 0, 0));
         Mat mskSharps(img.rows,img.cols, CV_8UC3, Scalar(0, 0, 0));
 
-            //Face boxes processing
+        //Face boxes processing
         float* faceData = (float*)(faceOut.data);
         for (size_t i = 0ul; i < faceOut.total(); i += faceObjectSize)
         {
@@ -129,8 +140,10 @@ int main(int argc, char** argv)
                 int faceHeightAdd = faceMaxSize - faceHeight;
 
                 Mat imgCrop;
-                cv::copyMakeBorder(img(Rect(faceLeft, faceTop, faceWidth, faceHeight)), imgCrop, faceHeightAdd / 2, (faceHeightAdd + 1) / 2,
-                                   faceWidthAdd / 2, (faceWidthAdd + 1) / 2, BORDER_CONSTANT | BORDER_ISOLATED , clrBlack);
+                cv::copyMakeBorder(img(Rect(faceLeft, faceTop, faceWidth, faceHeight)), imgCrop,
+                                   faceHeightAdd / 2, (faceHeightAdd + 1) / 2,
+                                   faceWidthAdd / 2, (faceWidthAdd + 1) / 2,
+                                   BORDER_CONSTANT | BORDER_ISOLATED , clrBlack);
 
                 //Infering Landmarks detector
                 landmNet.setInput(blobFromImage(imgCrop, 1.0, Size(landmCols, landmRows)));
@@ -142,8 +155,10 @@ int main(int argc, char** argv)
                 size_t j = 0ul;
                 for (; j < 18 * 2; j += 2)
                 {
-                    ptsFaceElems[j / 2] = Point(int(landmData[j] * imgCrop.cols + faceLeft - faceWidthAdd / 2),
-                                                int(landmData[j+1] * imgCrop.rows + faceTop - faceHeightAdd / 2));
+                    ptsFaceElems[j / 2] = Point(int(landmData[j] * imgCrop.cols + faceLeft -
+                                                    faceWidthAdd / 2),
+                                                int(landmData[j+1] * imgCrop.rows + faceTop -
+                                                    faceHeightAdd / 2));
                 }
 
                 std::vector<Point> vctFace;
@@ -151,19 +166,25 @@ int main(int argc, char** argv)
                     std::vector<Point> vctJaw;
                     for(; j < landmOut.total(); j += 2)
                     {
-                        vctJaw.push_back(Point(int(landmData[j] * imgCrop.cols + faceLeft - faceWidthAdd / 2),
-                                               int(landmData[j + 1] * imgCrop.rows + faceTop - faceHeightAdd / 2)));
+                        vctJaw.push_back(Point(int(landmData[j] * imgCrop.cols + faceLeft -
+                                                   faceWidthAdd / 2),
+                                               int(landmData[j + 1] * imgCrop.rows + faceTop -
+                                                   faceHeightAdd / 2)));
                     }
                     Point ptJawCenter((vctJaw[0] + vctJaw[16]) / 2);
-                    double angFace = atan((double)(vctJaw[8] - ptJawCenter).x / (double)(ptJawCenter - vctJaw[8]).y);
+                    double angFace = atan((double)(vctJaw[8] - ptJawCenter).x /
+                                          (double)(ptJawCenter - vctJaw[8]).y);
                     int jawWidth = int(norm(vctJaw[0] - vctJaw[16]));
                     int jawHeight = int(norm(ptJawCenter - vctJaw[8]));
                     double angForeheadStart = 180 - angFace * 180. / pi -
-                                              atan((double)(vctJaw[0] - ptJawCenter).y / (double)(ptJawCenter - vctJaw[0]).x) * 180. / pi;
+                                              atan((double)(vctJaw[0] - ptJawCenter).y /
+                                                   (double)(ptJawCenter - vctJaw[0]).x) * 180. / pi;
                     double angForeheadEnd = 360 - angFace * 180. / pi -
-                                            atan((double)(ptJawCenter - vctJaw[16]).y / (double)(vctJaw[16] - ptJawCenter).x)*180. / pi;
-                    ellipse2Poly(ptJawCenter, Size(jawWidth / 2, int(jawHeight / 1.5)), int(angFace * 180. / pi),
-                            int(angForeheadStart), int(angForeheadEnd), 1, vctFace);
+                                            atan((double)(ptJawCenter - vctJaw[16]).y /
+                                                 (double)(vctJaw[16] - ptJawCenter).x)*180. / pi;
+                    ellipse2Poly(ptJawCenter, Size(jawWidth / 2, int(jawHeight / 1.5)),
+                                 int(angFace * 180. / pi), int(angForeheadStart),
+                                 int(angForeheadEnd), 1, vctFace);
                     size_t lenJaw = vctJaw.size();
                     for (size_t k = 0ul; k < lenJaw; k++)
                     {
@@ -174,8 +195,11 @@ int main(int argc, char** argv)
                 std::vector<Point> vctLeftEye;
                 {
                     Point ptLeftEyeCenter((ptsFaceElems[0] + ptsFaceElems[1]) / 2);
-                    double angLeftEye = atan((double)(ptsFaceElems[0] - ptsFaceElems[1]).y / (double)(ptsFaceElems[0] - ptsFaceElems[1]).x) * 180. / pi;
-                    ellipse2Poly(ptLeftEyeCenter, Size(int(norm(ptsFaceElems[0] - ptsFaceElems[1]) / 2), int(norm(ptsFaceElems[0] - ptsFaceElems[1]) / 6)),
+                    double angLeftEye = atan((double)(ptsFaceElems[0] - ptsFaceElems[1]).y /
+                                        (double)(ptsFaceElems[0] - ptsFaceElems[1]).x) * 180. / pi;
+                    ellipse2Poly(ptLeftEyeCenter,
+                                 Size(int(norm(ptsFaceElems[0] - ptsFaceElems[1]) / 2),
+                                      int(norm(ptsFaceElems[0] - ptsFaceElems[1]) / 6)),
                                  int(angLeftEye), 0, 180, 1, vctLeftEye);
                 }
                 vctLeftEye.push_back(ptsFaceElems[12]);
@@ -185,8 +209,11 @@ int main(int argc, char** argv)
                 std::vector<Point> vctRightEye;
                 {
                     Point ptRightEyeCenter((ptsFaceElems[2] + ptsFaceElems[3]) / 2);
-                    double angRightEye = atan((double)(ptsFaceElems[3] - ptsFaceElems[2]).y / (double)(ptsFaceElems[3] - ptsFaceElems[2]).x) * 180. / pi;
-                    ellipse2Poly(ptRightEyeCenter, Size(int(norm(ptsFaceElems[3] - ptsFaceElems[2]) / 2), int(norm(ptsFaceElems[3] - ptsFaceElems[2]) / 6)),
+                    double angRightEye = atan((double)(ptsFaceElems[3] - ptsFaceElems[2]).y /
+                                         (double)(ptsFaceElems[3] - ptsFaceElems[2]).x) * 180. / pi;
+                    ellipse2Poly(ptRightEyeCenter,
+                                 Size(int(norm(ptsFaceElems[3] - ptsFaceElems[2]) / 2),
+                                      int(norm(ptsFaceElems[3] - ptsFaceElems[2]) / 6)),
                                  int(angRightEye), 0, 180, 1, vctRightEye);
                 }
                 vctRightEye.push_back(ptsFaceElems[15]);
@@ -203,10 +230,15 @@ int main(int argc, char** argv)
                 {
                     std::vector<Point> vctMouthTop;
                     Point ptMouthCenter((ptsFaceElems[8] + ptsFaceElems[9]) / 2);
-                    double angMouth = atan((double)(ptsFaceElems[9] - ptsFaceElems[8]).y / (double)(ptsFaceElems[9] - ptsFaceElems[8]).x) * 180. / pi;
-                    ellipse2Poly(ptMouthCenter, Size(int(norm(ptsFaceElems[9] - ptsFaceElems[8]) / 2), int(norm(ptMouthCenter - ptsFaceElems[11]))),
+                    double angMouth = atan((double)(ptsFaceElems[9] - ptsFaceElems[8]).y /
+                                      (double)(ptsFaceElems[9] - ptsFaceElems[8]).x) * 180. / pi;
+                    ellipse2Poly(ptMouthCenter,
+                                 Size(int(norm(ptsFaceElems[9] - ptsFaceElems[8]) / 2),
+                                      int(norm(ptMouthCenter - ptsFaceElems[11]))),
                                  int(angMouth), 0, 180, 1, vctMouth);
-                    ellipse2Poly(ptMouthCenter, Size(int(norm(ptsFaceElems[9] - ptsFaceElems[8]) / 2), int(norm(ptMouthCenter - ptsFaceElems[10]))),
+                    ellipse2Poly(ptMouthCenter,
+                                 Size(int(norm(ptsFaceElems[9] - ptsFaceElems[8]) / 2),
+                                      int(norm(ptMouthCenter - ptsFaceElems[10]))),
                                  int(angMouth), 180, 360, 1, vctMouthTop);
                     size_t lenMouthTop = vctMouthTop.size();
                     for (size_t k = 0ul; k < lenMouthTop; k++)
@@ -232,31 +264,29 @@ int main(int argc, char** argv)
                 GaussianBlur(mskBlur, mskBlurGaussed, Size(5, 5), 0);
                 GaussianBlur(mskSharp, mskSharpGaussed, Size(5, 5), 0);
                 GaussianBlur(mskFace, mskFaceGaussed, Size(5, 5), 0);
-                mskBlurGaussed -= mskBlurGaussed.mul(mskSharpGaussed);
                 threshold(mskBlurGaussed, mskBlurGaussed, 0, 1, THRESH_BINARY);
                 threshold(mskSharpGaussed, mskSharpGaussed, 0, 1, THRESH_BINARY);
                 threshold(mskFaceGaussed, mskFaceGaussed, 0, 1, THRESH_BINARY);
-                mskFaces = mskFaces - mskFaces.mul(mskFaceGaussed) + mskFaceGaussed;
-                mskBlurs = mskBlurs - mskBlurs.mul(mskBlurGaussed) + mskBlurGaussed;
+                mskNoFaces -= mskFaceGaussed;
                 mskSharps = mskSharps - mskSharps.mul(mskSharpGaussed) + mskSharpGaussed;
+                mskBlurs = mskBlurs - mskBlurs.mul(mskBlurGaussed) + mskBlurGaussed;
+                mskBlurs -= mskBlurs.mul(mskSharps);
 
-//Uncomment the following section to draw face box and facial landmarks on the input image
-//in a separate window
-
-//                Mat imgDraw;
-//                img.copyTo(imgDraw);
-//                {
-//                    std::vector<std::vector<Point>> vctvctContours;
-//                    vctvctContours.push_back(vctFace);
-//                    vctvctContours.push_back(vctLeftEye);
-//                    vctvctContours.push_back(vctRightEye);
-//                    vctvctContours.push_back(vctMouth);
-//                    vctvctContours.push_back(vctNose);
-//                    polylines(imgDraw, vctvctContours, true, clrYellow);
-//                }
-//                rectangle(imgDraw, Rect(faceLeft, faceTop, faceWidth, faceHeight), clrGreen, 1);
-//                namedWindow("Box and landmarks", WINDOW_NORMAL);
-//                imshow("Box and landmarks", imgDraw);
+                if (flgLandmarks == true)
+                {
+                    std::vector<std::vector<Point>> vctvctContours;
+                    vctvctContours.push_back(vctFace);
+                    vctvctContours.push_back(vctLeftEye);
+                    vctvctContours.push_back(vctRightEye);
+                    vctvctContours.push_back(vctMouth);
+                    vctvctContours.push_back(vctNose);
+                    polylines(imgDraw, vctvctContours, true, clrYellow);
+                }
+                if (flgBoxes == true)
+                {
+                    rectangle(imgDraw, Rect(faceLeft, faceTop, faceWidth, faceHeight),
+                              clrGreen, 1);
+                }
             }
             else
             {
@@ -269,12 +299,9 @@ int main(int argc, char** argv)
         bilateralFilter(img, imgBilat, 9, 30, 30);
         unsharpMask(img, imgSharp, 3, 1);
 
-        Mat mskNoFaces(img.rows,img.cols, CV_8UC3, Scalar(1, 1, 1));
-        mskNoFaces -= mskFaces;
-
         Mat imgShow = img.mul(mskNoFaces) + imgBilat.mul(mskBlurs) + imgSharp.mul(mskSharps);
 
-        imshow("Input", img);
-        imshow(winName, imgShow);
+        imshow(winInput, imgDraw);
+        imshow(winFB, imgShow);
     }
 }
